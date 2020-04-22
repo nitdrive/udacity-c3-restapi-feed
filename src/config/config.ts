416667c81f4bfa4aff1db7e3767482f2574cb6e4
Nithin <nithin.ayiluri@gmail.com>
2020-04-21 22:32:38 -0500

    Replaces env variables - temporarily

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": process.env.POSTGRES_USERNAME,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DATABASE,
-    "host": process.env.POSTGRES_HOST,
+    "username": "udagramdev",
+    "password": "udagramdev",
+    "database": "udagramdev",
+    "host": "udagramdev.c0q7jt1y7lps.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_reigion": process.env.AWS_REGION,
-    "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_BUCKET
+    "aws_reigion": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": "udacity-bucket-nd9990-filestore"
   },
   "prod": {
     "username": "",
@@ -17,7 +17,7 @@ export const config = {
     "dialect": "postgres"
   },
   "jwt": {
-    "secret": process.env.JWT_SECRET
+    "secret": "c0q7jt1y7lps"
   }
 
 }
